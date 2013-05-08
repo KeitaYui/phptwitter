ba33adc0b20ef30261b4e3e8573ce60f7ae1606b
YuiKeita <k.yui.ht@gmail.com>
2013-05-08 10:45:38 +0900

    プッシュ

---
+++ vagrant-codeigniter/codeigniter/application/views/mytwitter_main_view.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+<title>MainPage</title>
+
+</head>
+<body>
+Login ID: 
+<?php
+	if(isset($_SESSION["ID"])){
+		echo $_SESSION["ID"];
+	}
+?>
+
+<a id="test"></a>
+
+<form>
+<textarea id="textarea" cols="70" rows="2"></textarea>
+<input type ="button" id="tweetbutton" value="tweet">
+</form>
+
+<div id='tweetresult'></div>
+
+<script type="text/javascript" src="http://vagrant-codeigniter.local/js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript">
+
+var userID = "<?php echo $_SESSION['ID'];?>";
+var lastTweetNum = 0;
+var browserHeight = $(window).height();
+
+$("#tweetbutton").click(function(){
+	var str = $("#textarea").val();
+	$.post("http://vagrant-codeigniter.local/js/tweetsave.php",{text:str,ID:userID},function(json){
+		$("#test").append(" posted! ");
+		$("#tweetresult").prepend("</br>*" + str + "</br>----------------------------------------------------</br>");
+		$("#textarea").val("");
+	});
+});
+
+
+$(window).scroll(function(){
+	var scrollTop = $(document).scrollTop();
+	var tweetresultHeight = $("#tweetresult").height();
+	var tweetresultOffset = $("#tweetresult").offset().top;
+	if((tweetresultOffset + tweetresultHeight) < (scrollTop + browserHeight)){
+		$.getJSON("http://vagrant-codeigniter.local/js/tweets.json", function(data){
+			for(var i in data){
+				if(data[i].ID == userID){
+					for(var j = 0;j < 5;j++){
+						if(lastTweetNum > 0){
+							$("#tweetresult").append("</br>*" + data[i].tweet[lastTweetNum].str + "</br>----------------------------------------------------</br>");
+							lastTweetNum--;
+						}
+					}
+				}
+			}
+		});
+	}
+	else{
+	}
+});
+
+$(document).ready(function () {
+	$.getJSON("http://vagrant-codeigniter.local/js/tweets.json", function(data){
+		for(var i in data){
+			if(data[i].ID == userID){
+				lastTweetNum = data[i].maxNum;
+				for(var j = 0;j < 5;j++){
+					if(lastTweetNum > 0){
+						$("#tweetresult").append("</br>*" + data[i].tweet[lastTweetNum].str + "</br>----------------------------------------------------</br>");
+						lastTweetNum--;
+					}
+				}
+			}
+		}
+	});
+});
+</script>
+
+</body>
+</html>
